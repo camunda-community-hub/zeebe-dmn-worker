8b0c58aa47ee4a0e8d9d922ff236b4ef24ff3f6d
Philipp Ossler <philipp.ossler@gmail.com>
2017-08-18 03:46:41 +0200

    configure by properties

--- src/main/java/io/zeebe/dmn/StandaloneDmnApplication.java
+++ src/main/java/io/zeebe/dmn/StandaloneDmnApplication.java
@@ -1,23 +1,76 @@
+/*
+ * Copyright © 2017 camunda services GmbH (info@camunda.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.zeebe.dmn;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import java.util.Scanner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class StandaloneDmnApplication
 {
+    private static final Logger LOG = LoggerFactory.getLogger(StandaloneDmnApplication.class);
+
+    public static final String PROP_FILE = "application.properties";
+
+    public static final String REPO_DIR_PROP = "zeebe.dmn.repo";
+    public static final String TOPIC_PROP = "zeebe.dmn.topic";
 
     private static final String DEFAULT_REPO_DIR = "repo";
     private static final String DEFAULT_TOPIC = "default-topic";
 
     public static void main(String[] args)
     {
-        // TODO map args to client properties
+        final Properties properties = loadProperties();
 
-        final DmnApplication application = new DmnApplication(DEFAULT_REPO_DIR, DEFAULT_TOPIC);
+        final String repoDir = properties.getProperty(REPO_DIR_PROP, DEFAULT_REPO_DIR);
+        final String topic = properties.getProperty(TOPIC_PROP, DEFAULT_TOPIC);
+
+        final DmnApplication application = new DmnApplication(repoDir, topic);
         application.start();
 
         waitUntilClose();
 
         application.close();
+    }
+
+    private static Properties loadProperties()
+    {
+        final Properties properties = new Properties();
+
+        try
+        {
+            final InputStream propertyStream = StandaloneDmnApplication.class.getResourceAsStream(PROP_FILE);
+            if (propertyStream != null)
+            {
+                properties.load(propertyStream);
+            }
+            else
+            {
+                LOG.debug("No configuration found '{}'. Use default values.", PROP_FILE);
+            }
+        }
+        catch (IOException e)
+        {
+            LOG.warn("Failed to read properties '{}'.", PROP_FILE, e);
+        }
+        return properties;
     }
 
     private static void waitUntilClose()
