9bc99336d69b5344286658818e67f26c4e32e68d
Philipp Ossler <philipp.ossler@gmail.com>
2021-06-04 10:29:21 +0200

    build(deps): dump zeebe from 0.26.1 to 1.0.0
    * update the worker to the new Zeebe version

--- src/main/java/io/zeebe/dmn/DmnJobHandler.java
+++ src/main/java/io/zeebe/dmn/DmnJobHandler.java
@@ -15,10 +15,10 @@
  */
 package io.zeebe.dmn;
 
-import io.zeebe.client.api.response.ActivatedJob;
-import io.zeebe.client.api.worker.JobClient;
-import io.zeebe.client.api.worker.JobHandler;
-import io.zeebe.spring.client.annotation.ZeebeWorker;
+import io.camunda.zeebe.client.api.response.ActivatedJob;
+import io.camunda.zeebe.client.api.worker.JobClient;
+import io.camunda.zeebe.client.api.worker.JobHandler;
+import io.camunda.zeebe.spring.client.annotation.ZeebeWorker;
 import java.util.Collections;
 import java.util.Map;
 import org.camunda.bpm.dmn.engine.DmnDecision;
